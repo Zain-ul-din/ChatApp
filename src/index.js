734f6f7d4c16491921527f0ea9750bc1519f5a27
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-09-14 22:33:55 +0500

    Update index.js
    React18

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+/* React 18
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot (document.querySelector ('#root'))
+root.render (<><Main/></>);
+*/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
